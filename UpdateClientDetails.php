7f275db6752d22b9f2260a88fa84d211f8fc48e0
eshangulati <81874844+eshangulati@users.noreply.github.com>
2023-05-02 12:52:26 +1000

    Update UpdateClientDetails.php

--- UpdateClientDetails.php
+++ UpdateClientDetails.php
@@ -3,11 +3,11 @@
 <head>
     <meta charset="UTF-8">
     <meta name="description" content="Edit Client">
-    <meta name="keywords" content="Add Client Page, Oracle">
+    <meta name="keywords" content="Update Client Page, Oracle">
     <meta name="author" content="Eshan Gulati"/>
-    <title>AWS Add New Client</title>
+    <title>Edit Client Data</title>
     
-    <title>Job Application</title>
+    
     <!-- Bootstrap CSS -->
     <link
       href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css"
@@ -22,21 +22,7 @@
 </head>
 <body>
     <section id="EDITClient-Form">
-      <input
-        id="completionSlider"
-        type="range"
-        min="1"
-        max="111"
-        step="10"
-        value="0"
-        disabled
-      />
-
-       <form
-        method="POST"
-        action="https://mercury.swin.edu.au/it000000/formtest.php"
-        id="update-client"
-      >
+      <form name="form1" method="post" action="<?php echo $_SERVER['PHP_SELF']; ?>" >
 
       <div class = "container">
         <div class = "row">
@@ -61,7 +47,7 @@
 
     <div class="row">
         <div class="col-lg-6">
-            <label for="FName">First Name <span>*</span></label> <br />
+            <label for="fname">First Name <span>*</span></label> <br />
             <input
                 type="text"
                 name="First-Name"
@@ -76,7 +62,7 @@
 
     <div class = "row">
             <div class="col-lg-6">
-            <label for="LName">Last Name <span>*</span></label> <br />
+            <label for="lname">Last Name <span>*</span></label> <br />
             <input
                 type="text"
                 name="Last-Name"
@@ -88,109 +74,70 @@
             />
         </div>
     </div>
-  
-     <div class="row">
+
+    <div class="row">
             <div class="col-lg-6">
-              <label for="DOB">Date of Birth <span>*</span></label
-              ><br />
-              <input type="date" name="DOB" id="DOB" required />
-              <span id="DOB-err"></span>
+              <label for="email">Email Address<span>*</span></label> <br />
+              <input
+                type="email"
+                name="email"
+                id="email"
+                placeholder="Email Address"
+                required
+              />
             </div>
+
+    <div class="col-lg-6">
+              <label for="phone">Phone Number<span>*</span></label> <br />
+              <input
+                type="text"
+                name="phone"
+                id="phone"
+                pattern="[0-9 ]{8,12}"
+                placeholder="Phone Number"
+                required
+              />
+            </div>
+  
             <div class="col-lg-6">
               <fieldset>
                 <legend>Gender<span>*</span></legend>
                 <input
                   type="radio"
-                  id="Male"
+                  id="gender"
                   name="Gender"
-                  value="Male"
+                  value="M"
                   required
                 />Male
                 <input
                   type="radio"
-                  id="Female"
+                  id="gender"
                   name="Gender"
-                  value="Female"
+                  value="F"
                 />Female
                 <input
                   type="radio"
-                  id="Indeterminate/Intersex/Unspecified"
+                  id="gender"
                   name="Gender"
-                  value="Indeterminate/Intersex/Unspecified"
-                />Indeterminate/Intersex/Unspecified
+                  value="0"
+                />Other
               </fieldset>
             </div>
           </div>
-    
-    <div class="row">
-            <div class="col-lg-6">
-              <label for="Address">Street Address<span>*</span></label
-              ><br />
-              <input
-                type="text"
-                name="Street-Address"
-                id="Address"
-                pattern="{3,40}"
-                placeholder="Street Address"
-                required
-              />
-            </div>
-
-                        <div class="col-lg-6">
-              <label for="Town">Town/Suburb<span>*</span></label> <br />
-              <input
-                type="text"
-                name="Town"
-                id="Town"
-                pattern="{3,40}"
-                placeholder="Town/Suburb"
-                required
-              />
-            </div>
-          </div>
+  
           <div class="row">
             <div class="col-lg-6">
-              <label for="State">State<span>*</span></label
-              ><br />
-              <select name="State" id="State" required>
-                <option value="">Select</option>
-                <option value="VIC">VIC</option>
-                <option value="NSW">NSW</option>
-                <option value="QLD">QLD</option>
-                <option value="NT">NT</option>
-                <option value="WA">WA</option>
-                <option value="SA">SA</option>
-                <option value="TAS">TAS</option>
-                <option value="ACT">ACT</option>
-              </select>
-            </div>
-            <div class="col-lg-6">
-              <label for="Postcode">Postcode<span>*</span></label
-              ><br />
-              <input
-                type="text"
-                name="Postcode"
-                id="Postcode"
-                pattern="[0-9]{4}"
-                placeholder="Postcode"
-                required
-              />
-              <span id="Postcode-err"></span>
-            </div>
-          </div>
-          <div class="row">
-            <div class="col-lg-6">
-              <h1>Wealth Information</h1>
+              <h1>Financial Information</h1>
             </div>
           </div>
 
           <div class="row">
             <div class="col-lg-6">
-              <label for="FName">Income <span>*</span></label> <br />
+              <label for="income">Income <span>*</span></label> <br />
               <input
                 type="text"
                 name="Income"
-                id="FName"
+                id="income"
                 pattern="[0-9]{1,20}"
                 placeholder="Income"
                 size="25"
@@ -198,11 +145,11 @@
               />
             </div>
             <div class="col-lg-6">
-              <label for="LName">Net Worth <span>*</span></label> <br />
+              <label for="networth">Net Worth <span>*</span></label> <br />
               <input
                 type="text"
                 name="Net Worth"
-                id="LName"
+                id="networth"
                 pattern="[0-9]{1,20}"
                 placeholder="Net Worth"
                 size="25"
@@ -212,23 +159,35 @@
           </div>
           <div class="row">
             <div class="col-lg-6">
-              <label for="Email">Risk Tolerance<span>*</span></label> <br />
+              <label for="risktol">Risk Tolerance<span>*</span></label> <br />
               <input
                 type="text"
-                name="Risk"
-                id="Email"
+                name="Risk Tolerance"
+                id="risktol"
                 placeholder="Risk Tolerance"
                 required
               />
             </div>
             <div class="col-lg-6">
-              <label for="PhNum">Investmennt Goals<span>*</span></label> <br />
+              <label for="goals">Investmennt Goals<span>*</span></label> <br />
               <input
                 type="text"
                 name="Goals"
                 id="PhNum"
                 placeholder="Investmennt Goals"
                 required
+              />
+
+                    <!-- Search bar -->
+	            <h1>Client Search</h1>
+	            <h5>Enter Client ID to begin</h5>
+              <form name="search" method="get" action="<?php echo $_SERVER['PHP_SELF']; ?>" >
+              <label for="cID">Client ID: </label>
+              <input
+              type="text"
+              name="cID"
+              id="cID"
+              pattern="[0-9]"
               />
             </div>
           </div>
@@ -240,7 +199,111 @@
           </div>
         </div>
       </form>
-     
+      </section>
+	
+	<?php
+    if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+        // Get the updated client details from the POST data
+      $fname = $_POST['fname'];
+			$lname = $_POST['lname'];
+			$phone = $_POST['phone'];
+			$email = $_POST['email'];
+			$gender = $_POST['gender'];
+			$income = $_POST['income'];
+			$networth = $_POST['networth'];
+			$risktol = $_POST['risktol'];
+			$goals = $_POST['goals'];
+    
+        // Open the CSV file
+        $file = fopen('clients.csv', 'r+');
+    
+        // Lock the file for exclusive access
+        if (flock($file, LOCK_EX)) {
+            // Read the contents of the file into an array
+            $clients = array();
+            while (($line = fgetcsv($file)) !== FALSE) {
+                $clients[] = $line;
+            }
+    
+            // Find the client in the array by ID
+            foreach ($clients as &$c) {
+                if ($c[0] == $client_id) {
+                    // Update the client details
+                    $c[1] = $fname ;
+                    $c[2] = $lname;
+                    $c[3] = $phone;
+                    $c[4] = $email;
+                    $c[5] = $gender;
+                    $c[6] = $income;
+                    $c[7] = $networth;
+                    $c[8] = $risktol;
+                    $c[9] = $goals;
+
+                    break;
+                }
+            }
+    
+            // Write the updated client data back to the file
+            ftruncate($file, 0);
+            rewind($file);
+            foreach ($clients as $c) {
+                fputcsv($file, $c);
+            }
+    
+            // Release the file lock and close the file
+            flock($file, LOCK_UN);
+            fclose($file);
+    
+            // Redirect the user back to the client list page
+            header('Location: client_list.php');
+        } else {
+            echo 'Unable to lock file.';
+        }
+    } else {
+        // Open the CSV file
+        $file = fopen('clients.csv', 'r');
+    
+        // Read the contents of the file into an array
+        $clients = array();
+        while (($line = fgetcsv($file)) !== FALSE) {
+            $clients[] = $line;
+        }
+    
+        // Close the file
+        fclose($file);
+    
+        // Get the client ID from the query string
+        $client_id = $_GET['id'];
+    
+        // Find the client in the array by ID
+        $client = null;
+        foreach ($clients as $c) {
+            if ($c[0] == $client_id) {
+                $client = $c;
+                break;
+            }
+        }
+    
+        // Display the client details in a form for editing
+        if ($client) {
+            echo '<form method="post">';
+            echo '<input type="hidden" name="CID" value="' . $client[0] . '">';
+            echo 'Name: <input type="text" name="fname" value="' . $client[1] . '"><br>';
+            echo 'Last Name: <input type="text" name="lname" value="' . $client[2] . '"><br>';
+            echo 'Phone Number: <input type="text" name="phone" value="' . $client[3] . '"><br>';
+            echo 'Email Address: <input type="text" name="email" value="' . $client[4] . '"><br>';
+            echo 'Gender: <input type="radio" name="Gender" value"'. $client[5]. '"><br>';
+            echo 'Income: <input type="text" name ="Gender" value"'. $client[6]. '"><br>';
+            echo 'Net Worth: <input type="text" name="Net Worth value"'. $client[7]. '"><br>;';
+            echo 'Risk Tolerance: <input type="text" name="Risk Tolerance" value="' $client[8].'><br>';
+            echo 'Investment Goals:<input type="text" name="Investment Goals" value="'.$client[9]. '><br>';
+            echo '<input type="submit" value="Save">';
+            echo '</form>';
+        } else {
+            echo 'Client not found.';
+        }
+    }
+    ?>
 
     <footer>
       <div class="container-fluid">
