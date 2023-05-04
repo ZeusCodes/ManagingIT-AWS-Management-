a7edea0894239e48b0052ce3c8fc728a7e62c3a2
udgrover16 <100520126+udgrover16@users.noreply.github.com>
2023-05-04 19:16:51 +1000

    Update UpdataPortfolio.php

--- Website/UpdataPortfolio.php
+++ Website/UpdataPortfolio.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="description" content="Update Portfolio">
     <meta name="keywords" content="Update Portfolio Page, Oracle">
-    <meta name="author" content="Eshan Gulati"/>
+    <meta name="author" content="Uddhav Grover"/>
     <title>Register New Investment Details</title>
 
         <!-- Bootstrap CSS -->
@@ -19,8 +19,9 @@
       <link rel="stylesheet" href="./styles/styles.css" />
 
 </head>
+
 <body>
-<header>
+  <header>
       <nav
         class="navbar navbar-expand-lg navbar-dark"
         style="background-color: #394867"
@@ -57,18 +58,13 @@
                 >
               </li>
               <li class="nav-item">
-                <a class="nav-link" href="./UpdateClientDetails.php"
+                <a class="nav-link" href="./UpdateClientDetails.html"
                   >Update Client</a
                 >
               </li>
               <li class="nav-item">
                 <a class="nav-link" href="./portfolioReg.php"
-                  >Register New Investment</a
-                >
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="./UpdataPortfolio.php"
-                  >Update Portfolio Investment</a
+                  >Update Client Portfolio</a
                 >
               </li>
             </ul>
@@ -76,183 +72,102 @@
         </div>
       </nav>
     </header>
-     <section id="Application-Form">
 
-      <form name="form1" method="post" action="<?php echo $_SERVER['PHP_SELF']; ?>" >
-
-        <div class = "container">
-          <div class = "row">
-              <div class ="col-lg-6">
-                  <h1>Portfolio Information</h1>
-              </div>
-          </div>     
-        </div>
-
-
-        <div class="row">
-          <div class="col-lg-6">
-              <label for="IType">Investmennt Type <span>*</span></label> <br />
-              <input
-                  type="text"
-                  name="Investmennt-Type"
-                  id="IType"
-                  pattern="[A-Za-z]{1,20}"
-                  placeholder="Investmennt Type"
-                  required
-              />
-          </div>
-        </div>
-
-        <div class="row">
-          <div class="col-lg-6">
-              <label for="pNoShares">Number of Shares <span>*</span></label> <br />
-              <input
-                  type="number"
-                  name="Number-Shares"
-                  id="pNoShares"
-                  placeholder="Number of Shares"
-                  required
-              />
-          </div>
-        </div>
-
-        <div class="row">
-          <div class="col-lg-6">
-              <label for="pBuyDate">Date Bought <span>*</span></label> <br />
-              <input
-                  type="date"
-                  name="Date-Bought"
-                  id="pBuyDate"
-                  required
-              />
-              <span id="DOB-err"></span>
-          </div>
-        </div>
-
-        <div class="row">
-          <div class="col-lg-6">
-              <label for="pBuyPrice">Price <span>*</span></label> <br />
-              <input
-                  type="number"
-                  name="Price"
-                  id="pBuyPrice"
-                  placeholder="Price"
-                  required
-              />
-          </div>
-        </div>
-
-        <div class="row">
-          <div class="col-lg-6">
-              <label for="pCurrentVal">Current Value <span>*</span></label> <br />
-              <input
-                  type="number"
-                  name="Current-Value"
-                  id="pCurrentVal"
-                  placeholder="Current Value"
-                  required
-              />
-          </div>
-        </div>
-
-        <div class="row">
-          <div class="col-lg-6">
-              <label for="pInterest">Interest <span>*</span></label> <br />
-              <input
-                  type="number"
-                  name="Interest"
-                  id="pInterest"
-                  placeholder="Interest"
-                  required
-              />
-          </div>
-        </div>
-
-        <div class="row">
-          <div class="col-lg-6">
-              <label for="pCaptial">Captial <span>*</span></label> <br />
-              <input
-                  type="number"
-                  name="Capital"
-                  id="pCapital"
-                  placeholder="Capital"
-                  required
-              />
-          </div>
-        </div>
-
-        <h1>Client Search</h1>
-	            <h5>Enter Client ID to begin</h5>
-              <form name="search" method="get" action="<?php echo $_SERVER['PHP_SELF']; ?>" >
-              <label for="cID">Client ID: </label>
-              <input
-              type="text"
-              name="cID"
-              id="cID"
-              pattern="[0-9]"
-              />
-
-      </div>
-      <div class="row">
-        <div class="col-lg-6">
-          <span id="Form-err"></span><br />
-          <button type="submit" class="btn btn-primary">Update</button>
-        </div>
-      </div>
-    </div>
-    </form>
-    </section>
+    <form method="post" action="" style="margin:100px">
+    <h1>Portfolio Search and Edit</h1>
+		<label for="client-id">Enter Client ID:</label>
+		<input type="text" name="client-id" id="client-id" required>
+		<input type="submit" name="search" value="Search">
+	</form>
+	<form method="post" action="" style="margin:50px">
+    <h1>Client Search and Edit</h1>
+		<label for="client-id">Enter Client ID:</label>
+		<input type="text" name="client-id" id="client-id" required>
+		<input type="submit" name="search" value="Search">
+	</form>
 	
 	<?php
+	$portfolioFile = 'database/portfolio.csv';
+	if(isset($_POST['search'])) {
+		$client_id = $_POST['client-id'];
+		$portfolios = array_map('str_getcsv', file($portfolioFile));
+		$found_portfolio = false;
+		foreach($portfolios as $portfolio) {
+			if($portfolio[1] == $client_id) {
+				$found_portfolio = true;
+				$portfolio_id = $portfolio[0]
+				$client_id = $portfolio[1];
+				$inv_type = $portfolio[2];
+				$number_shares = $portfolio[3];
+				$buy_date = $portfolio[4];
+				$buy_price = $portfolio[5];
+				$buy_price = $portfolio[5];
+				$current_val = $portfolio[6];
+				$interest = $portfolio[7];
+				$capital = $portfolio[8];
 
-if(isset($_GET['search'])) {
-  // Open the CSV file
-  $file = fopen('database/portfolio.csv', 'r');
-  
-  // Find the row that matches the ID in the URL
-  while (($row = fgetcsv($file)) !== false) {
-      if ($row[0] == $_GET['search']) {
-          $data = $row;
-          break;
+				break;
+			}
+		}
+		if ($found_portfolio) {
+			echo '<form id="Portfolio-Update-Form" method="post" action="">';
+  			echo '<label for="client-id" style="margin: 10px; padding: 10px;">Client id:</label>';
+  			echo '<input type="text" name="client-id" id="client-id" value="'.$client_id.'" required><br>';
+  			echo '<label for="inv-type" style="margin: 10px; padding: 10px;">Investment Type Type:</label>';
+ 	 		echo '<input type="text" name="inv-type" id="inv-type" value="'.$inv_type.'" required><br>';
+  			echo '<label for="number-shares" style="margin: 10px; padding: 10px;">Number of shares:</label>';
+  			echo '<input type="int" name="number-shares" id="number-shares" value="'.$number_shares.'" required><br>';
+  			echo '<label for="buy-date" style="margin: 10px; padding: 10px;">Date Bought:</label>';
+  			echo '<input type="date" name="buy-date" id="buy-date" value="'.$buy_date.'" required><br>';
+ 		 	echo '<label for="buy-price" style="margin: 10px; padding: 10px;">Buy Price:</label>';
+  			echo '<input type="number" name="buy-price" id="buy-price" value="'.$buy_price.'" required><br>';
+ 		 	echo '<label for="current-val" style="margin: 10px; padding: 10px;">Current Value:</label>';
+  			echo '<input type="number" name="current-val" id="current-val" value="'.$current_val.'" required><br>';
+  			echo '<label for="interest" style="margin: 10px; padding: 10px;">Interest:</label>';
+ 			echo '<input type="number" name="interest" id="interest" value="'.$interest.'" required><br>';
+  			echo '<label for="capital" style="margin: 10px; padding: 10px;">Capital:</label>';
+  			echo '<input type="number" name="capital" id="capital" value="'.$capital.'" required><br>';
+  			echo '<input type="hidden" name="portfolio-id" value="'.$portfolio_id.'">';
+  			echo '<input type="submit" name="save" value="Save">';
+ 			echo '</form>';
+		} else {
+			echo 'Portfolio not found.';
+		}
+	}
+	$portfolioFile = 'database/portfolio.csv';
+	if(isset($_POST['save'])) {
+		$portfolio_id = $_POST['portfolio-id'];
+		$client_id = $_POST['client-id'];
+		$inv_type = $_POST['inv-type'];
+		$number_shares = $_POST['number-shares'];
+		$buy_date = $_POST['buy-date'];
+		$buy_price = $_POST['buy-price'];
+		$current_val = $_POST['current-val'];
+		$interest = $_POST['interest'];
+		$capital = $_POST['capital'];
+		$portfolios = array_map('str_getcsv', file($portfolioFile));
+		$updated_portfolio = false;
+		foreach($portfolios as &$portfolio) {
+			if($portfolio[0] == $portfolio_id) {
+				$portfolio[1] = $client_name;
+        	$updated_portfolio = true;
+        break;
       }
-  }
-  
-  // Close the CSV file
-  fclose($file);
-}
-if(isset($_POST['submit'])) {
-  // Open the CSV file in read/write mode
-  $file = fopen('database/portfolio.csv', 'r+');
-  
-  // Find the row that matches the ID in the URL and update it
-  while (($row = fgetcsv($file)) !== false) {
-      if ($row[0] == $_POST['cID']) {
-          $row[1] = $_POST['IType'];
-          $row[2] = $_POST['pNoShares'];
-          $row[3] = $_POST['pBuyDate'];
-          $row[4] = $_POST['pBuyPrice'];
-          $row[5] = $_POST['pCurrentVal'];
-          $row[6] = $_POST['pInterest'];
-          $row[7] = $_POST['pCapital'];
-          
-          
-          // Go back to the beginning of the row and write the updated data
-          fseek($file, -strlen(implode(',', $row)), SEEK_CUR);
-          fputcsv($file, $row);
-          
-          // Stop searching for the row
-          break;
+    }
+    if($updated_portfolio) {
+      $fp = fopen($portfolioFile, 'w');
+      foreach($portfolios as $portfolio) {
+        fputcsv($fp, $portfolio);
       }
+      fclose($fp);
+      echo "Portfolio for client $client_id has been updated.";
+    } else {
+      echo "Portfolio for client $client_id could not be found.";
+    }
+  } else {
+    echo "All updates must be provided.";
   }
-  
-  // Close the CSV file
-  fclose($file);
-  
-  // Redirect back to the original page
-  header('Location: clientDetails.php?id=' . $_POST['id']);
-  exit;
-}
-	?>
+  ?>
 
     <footer>
       <div class="container-fluid">
@@ -272,8 +187,7 @@ if(isset($_POST['submit'])) {
       integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM"
       crossorigin="anonymous"
     ></script>
-  </body>
-</html>
+
     <script src="./scripts/apply.js"></script>
     <script src="./scripts/dropdown.js"></script>
     <!-- Option 1: Bootstrap Bundle with Popper -->
